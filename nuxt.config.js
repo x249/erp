1b9d08740aea10a090fa25ecee9f84d4053ab69f
phr3nzy <adilosama47@gmail.com>
2019-05-16 09:51:53 +0200

    Add: chart.js plugin and configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/vuetify'],
+  plugins: ['@/plugins/vuetify', '@/plugins/chart'],
 
   /*
    ** Nuxt.js modules
