a4d1166557160e1e54a288e2c2c3ace74ea86b90
phr3nzy <adilosama47@gmail.com>
2019-05-16 11:55:07 +0200

    Add: nav-drawer items

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,16 +24,35 @@
             <v-list-tile-title v-text="item.title" />
           </v-list-tile-content>
         </v-list-tile>
+
+        <v-list-group prepend-icon="security">
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-title>Admin</v-list-tile-title>
+            </v-list-tile>
+          </template>
+          <v-list-tile v-for="(page, i) in adminPages" :key="i" :to="page.to">
+            <v-list-tile-action>
+              <v-icon>{{ page.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ page.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
+
     <v-toolbar color="primary" :clipped-left="true" dark flat fixed app>
       <v-toolbar-side-icon @click="miniVariant = !miniVariant" />
     </v-toolbar>
+
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
+
     <v-footer color="primary" dark :fixed="fixed" app>
       <v-flex text-xs-center>
         <span>&copy; 2019</span>
@@ -57,9 +76,31 @@ export default {
           to: '/dashboard'
         },
         {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'calendar_today',
+          title: 'Calendar',
+          to: '/calendar'
+        },
+        {
+          icon: 'assignment_ind',
+          title: 'Employees',
+          to: '/employees'
+        },
+        {
+          icon: 'monetization_on',
+          title: 'Finance',
+          to: '/finance'
+        }
+      ],
+      adminPages: [
+        {
+          icon: 'account_circle',
+          title: 'Users',
+          to: '/admin/users'
+        },
+        {
+          icon: 'settings',
+          title: 'Settings',
+          to: '/admin/settings'
         }
       ],
       title: 'Vuetify.js'
