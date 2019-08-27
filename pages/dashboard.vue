0e5202e986661b1d3170b2da4313472d58c1deb4
phr3nzy <adilosama47@gmail.com>
2019-08-27 19:54:48 +0200

    Edit: add custom analytics cards

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,7 +1,128 @@
 <template>
-  <v-layout row wrap>
+  <v-layout class="py-5" row>
     <v-flex xs12 md4>
-      <v-card class="pa-3 ma-3 slight-br" hover flat>
+      <v-card
+        class="mt-5 mx-auto"
+        flat
+
+        max-width="400"
+      >
+        <v-sheet
+          class="v-sheet--offset mx-auto px-4 py-2"
+          color="#F44336"
+          elevation="12"
+          max-width="calc(100% - 32px)"
+        >
+          <v-sparkline
+            :labels="labels"
+            :value="value"
+            color="white"
+            line-width="2"
+            padding="16"
+          />
+        </v-sheet>
+        <v-card-text class="pt-0">
+          <div class="title font-weight-light mb-2">
+            Server Traffic Load
+          </div>
+          <div class="subheading font-weight-light green--text">
+            Cluster Autoscaling Active <v-icon color="green">
+              {{ mdiGaugeFull }}
+            </v-icon>
+          </div>
+          <v-divider class="my-2" />
+          <v-icon
+            class="mr-2"
+            small
+          >
+            {{ mdiClock }}
+          </v-icon>
+          <span class="caption grey--text font-weight-light">last pinged 2 minutes ago</span>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 md4>
+      <v-card
+        class="mt-5 mx-auto"
+        flat
+
+        max-width="400"
+      >
+        <v-sheet
+          class="v-sheet--offset mx-auto px-4 py-2"
+          color="cyan"
+          elevation="12"
+          max-width="calc(100% - 32px)"
+        >
+          <v-sparkline
+            :labels="labels"
+            :value="value"
+            color="white"
+            line-width="2"
+            padding="16"
+          />
+        </v-sheet>
+        <v-card-text class="pt-0">
+          <div class="title font-weight-light mb-2">
+            User Registrations
+          </div>
+          <div class="subheading font-weight-light grey--text">
+            Last Campaign Performance
+          </div>
+          <v-divider class="my-2" />
+          <v-icon
+            class="mr-2"
+            small
+          >
+            {{ mdiClock }}
+          </v-icon>
+          <span class="caption grey--text font-weight-light">last registration 26 minutes ago</span>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 md4>
+      <v-card
+        class="mt-5 mx-auto"
+        flat
+
+        max-width="400"
+      >
+        <v-sheet
+          class="v-sheet--offset mx-auto px-4 py-2"
+          color="#9C27B0"
+          elevation="12"
+          max-width="calc(100% - 32px)"
+        >
+          <v-sparkline
+            :labels="labels"
+            :value="value"
+            color="white"
+            line-width="2"
+            padding="16"
+          />
+        </v-sheet>
+        <v-card-text class="pt-0">
+          <div class="title font-weight-light mb-2">
+            Daily Profit
+          </div>
+          <div class="subheading font-weight-light green--text">
+            Increasing <v-icon color="green">
+              {{ mdiFinance }}
+            </v-icon>
+          </div>
+          <v-divider class="my-2" />
+          <v-icon
+            class="mr-2"
+            small
+          >
+            {{ mdiClock }}
+          </v-icon>
+          <span class="caption grey--text font-weight-light">last calculated a minute ago</span>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 md4>
+      <v-card class="pa-3 ma-3" flat>
         <client-only>
           <chartjs-pie
             :width="100"
@@ -15,7 +136,7 @@
       </v-card>
     </v-flex>
     <v-flex xs12 md4>
-      <v-card class="pa-3 ma-3 slight-br" hover flat>
+      <v-card class="pa-3 ma-3" flat>
         <client-only>
           <chartjs-line
             :height="300"
@@ -29,7 +150,7 @@
       </v-card>
     </v-flex>
     <v-flex xs12 md4>
-      <v-card class="pa-3 ma-3 slight-br" hover flat>
+      <v-card class="pa-3 ma-3" flat>
         <client-only>
           <chartjs-radar
             :height="300"
@@ -45,11 +166,35 @@
 </template>
 
 <script>
+import { mdiClock, mdiFinance, mdiGaugeFull } from '@mdi/js'
 export default {
   head: () => ({
     title: 'Dashboard'
   }),
   data: () => ({
+    mdiClock,
+    mdiFinance,
+    mdiGaugeFull,
+    labels: [
+      '12am',
+      '3am',
+      '6am',
+      '9am',
+      '12pm',
+      '3pm',
+      '6pm',
+      '9pm'
+    ],
+    value: [
+      200,
+      675,
+      410,
+      390,
+      310,
+      460,
+      250,
+      800
+    ],
     options: {
       responsive: true,
       maintainAspectRatio: true,
@@ -80,3 +225,10 @@ export default {
   })
 }
 </script>
+
+<style>
+  .v-sheet--offset {
+    top: -24px;
+    position: relative;
+  }
+</style>
